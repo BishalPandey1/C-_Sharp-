92e84f620bbdbb0d675e2d0ea90cea803e35b20e
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-11-24 22:29:43 +0545

    Add files via upload

---
+++ Simple_Intreset_Program/more.cs
@@ -0,0 +1,9 @@
+using System;
+class Home
+{
+    void room()
+    {
+               Console.Write("Enter Principal amount: ");
+        double principal = Convert.ToDouble(Console.ReadLine());
+    }
+}
