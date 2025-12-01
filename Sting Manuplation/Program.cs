e0332ff9f869dfb19fcabcbaac15d243488e4de7
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-12-01 21:40:49 +0545

    Add files via upload

--- Sting Manuplation/Program.cs
+++ Sting Manuplation/Program.cs
@@ -10,28 +10,27 @@ class Program
         
         // Concatenation
         string greeting = "Hello " + name;
-        Console.WriteLine(greeting);  // Output: Hello Bishal
-
+        Console.WriteLine(greeting); 
         // Length of string
-        Console.WriteLine("Length: " + name.Length);  // Output: 6
+        Console.WriteLine("Length: " + name.Length); 
 
         // Substring
-        Console.WriteLine("First 3 letters: " + name.Substring(0, 3));  // Output: Bis
+        Console.WriteLine("First 3 letters: " + name.Substring(0, 3));  
 
         // Uppercase & Lowercase
-        Console.WriteLine("Uppercase: " + name.ToUpper());  // Output: BISHAL
-        Console.WriteLine("Lowercase: " + name.ToLower());  // Output: bishal
+        Console.WriteLine("Uppercase: " + name.ToUpper());  
+        Console.WriteLine("Lowercase: " + name.ToLower());  
 
         // Replace
         string replaced = name.Replace("al", "u");
-        Console.WriteLine("After replace: " + replaced);  // Output: Bishu
+        Console.WriteLine("After replace: " + replaced); 
 
         // Split
         string sentence = "C# is fun to learn";
         string[] words = sentence.Split(' ');
-        Console.WriteLine("First word: " + words[0]);  // Output: C#
+        Console.WriteLine("First word: " + words[0]); 
 
         // Searching
-        Console.WriteLine("Contains 'fun'? " + sentence.Contains("fun"));  // Output: True
+        Console.WriteLine("Contains 'fun'? " + sentence.Contains("fun"));  c
     }
 }
---
+++ StingBuilder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class StringBuilder
+{
+    static void main()
+    { 
+        //append
+         StringBuilder sb = new StringBuilder("Hello");
+         sb.Append("Bishal");
+         Console.WriteLine(sb.ToString());
+
+// Insert
+         StringBuilder sb = new StringBuilder("Hello World!");
+         sb.Insert(6,"Beutiful");
+         Console.WriteLine(sb.ToString());
+
+         // Remove
+         StringBuilder sb = new StringBuilder("Hello Beautful World! ");
+         sb.Remove(6,10);
+         Console.WriteLine(sb.ToString());
+
+    }
+}
