9fd586bf0c8418b312932d8a94ac9ebf2b706a6f
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2026-01-19 23:12:46 +0545

    Add files via upload

---
+++ Data Types/Program.cs
@@ -0,0 +1,24 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// using system
+using System;
+namespace MyApplication
+{
+    class Datatype
+    {
+        static void Main(String[] args)
+        {
+            int myNumber = 5 ;
+            double myDouble = 5.99;
+            char mychar = 'b';
+            bool theboolen = true ;
+            string mytext = "Bishal";
+            Console.WriteLine("This are the different data type use in C# ");
+            Console.WriteLine("The int value is:"+myNumber);
+            Console.WriteLine("The value of double is: "+myDouble);
+            Console.WriteLine("The value of char is: "+mychar);
+            Console.WriteLine("The value of boolen is: "+theboolen);
+            Console.WriteLine("The value of Char is: "+mytext);
+        }
+    }
+}
