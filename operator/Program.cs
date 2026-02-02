e7cb1d9bee53e7c34882621de1d78ee417d92a53
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2026-02-02 14:07:20 +0545

    Add files via upload

---
+++ operator/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+Console.WriteLine("Arithmetic Operators perform common mathematical operations.");
+
+int x = 10; 
+int y = 5;
+Console.WriteLine(x+y);
+Console.WriteLine(x-y);
+Console.WriteLine(x/y);
+Console.WriteLine(x%y);
+
+Console.WriteLine("Relation Operators compare two values.");
+
+int a = 10;
+int b = 20;
+Console.WriteLine(a==b);
+Console.WriteLine(a != b);
+Console.WriteLine(a > b);
+Console.WriteLine(a < b);
+
+Console.WriteLine("Logical Operators perform bit-level operations.");
+bool p= true, q = false;
+Console.WriteLine(p && q);
+Console.WriteLine(p || q);
+Console.WriteLine(!p);
+
+Console.WriteLine("Bitwise Operators perform bit-level operations.");
+int m = 5;
+int n = 3;
+Console.WriteLine(m & n);
+Console.WriteLine(m | n);
+Console.WriteLine(m ^ n );
+
+Console.WriteLine("The conditional operator returns one of two value base on a Boolean expression.");
+int d = (a > b ) ? a : b;
+Console. WriteLine(d);
+
